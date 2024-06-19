65d5b4b6912a1c836ba30fefffc854a0b87600df
Ruslan <lelyimalikrusik12301@gmail.com>
2024-06-19 17:35:30 +0300

    New spring project

---
+++ src/main/java/com/shop/tigranruslanshop/TigranRuslanShopApplication.java
@@ -0,0 +1,13 @@
+package com.shop.tigranruslanshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TigranRuslanShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TigranRuslanShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shop/tigranruslanshop/TigranRuslanShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.tigranruslanshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TigranRuslanShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
